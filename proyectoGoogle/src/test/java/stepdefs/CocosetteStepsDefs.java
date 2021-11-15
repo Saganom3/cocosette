f40100c6c8b59c7bb68d45571c14ceadb56eac1a
Saganom3 <Saga3692>
2021-11-15 17:57:35 -0500

    Nueva actualizacion 3.0

--- proyectoGoogle/src/test/java/stepdefs/CocosetteStepsDefs.java
+++ proyectoGoogle/src/test/java/stepdefs/CocosetteStepsDefs.java
@@ -1,8 +1,6 @@
 package stepdefs;
 
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
+import cucumber.api.java.en.*;
 import pages.CocosettePage;
 
 public class CocosetteStepsDefs {
